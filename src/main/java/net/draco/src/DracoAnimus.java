6be10874281a2e6b23ff992da61a52f24b42c84e
MCrafted <zacharynwogwugwu@hotmail.co.uk>
2015-01-07 12:00:22 +0000

    Delete DracoAnimus.java
    Because there is already a main mod file.

--- src/main/java/net/draco/src/DracoAnimus.java
+++
@@ -1,8 +0,0 @@
-//Add code here.
-package net.draco.src;
-@Mod(modid = "draco", version = "0.02", name = "Draco Animus")
-public class DracoAnimus {
-  public void init(FMLInitializationEvent event) {
-    Blocks.init()
-  }
-}
