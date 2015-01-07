534332ba20356f790fe1b5bcafcf598e86e9a7f7
MCrafted <zacharynwogwugwu@hotmail.co.uk>
2015-01-07 09:51:29 +0000

    Update and rename Blocks to Blocks.java

--- src/main/java/net/draco/src/Blocks.java
+++ src/main/java/net/draco/src/Blocks.java
@@ -8,15 +8,15 @@ import net.minecraftforge.common.MinecraftForge;
 
 public class Draco_Animus_ModBlocks {
 	
-	public static Block Charred_Ground;
- 	public static Block Scorched_Cobblestone;	
+	public static Block charredGround;
+ 	public static Block scorchedCobblestone;	
 	
 	public static void BlockInformation() {
 			
 		Charred_Ground = new Draco_Animus_ModForgeBlocks(Material.grass).setHardness(1.0F).setResistance(3.0F).setBlockName("charredGround").setCreativeTab(OthersDraco_Animus.TabDraco_Animus);
 		Scorched_Cobblestone = new Draco_Animus_ModForgeBlocks(Material.rock).setHardness(5.0F).setResistance(3.0F).setBlockName("scorchedCobblestone").setCreativeTab(OthersDraco_Animus.TabDraco_Animus);
 				
-		GameRegistry.registerBlock(charredGround, "Charred Ground");
-		GameRegistry.registerBlock(scorchedCobblestone, "Scorched Cobblestone");
+		GameRegistry.registerBlock(charredGround, "charredGround");
+		GameRegistry.registerBlock(scorchedCobblestone, "scorchedCobblestone");
 	}
 }
