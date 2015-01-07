01f1b644e4b83834ba8e62d81cffd273d2f120ff
MCrafted <zacharynwogwugwu@hotmail.co.uk>
2015-01-07 09:53:47 +0000

    Update Blocks.java

--- src/main/java/net/draco/src/Blocks.java
+++ src/main/java/net/draco/src/Blocks.java
@@ -11,7 +11,7 @@ public class Draco_Animus_ModBlocks {
 	public static Block charredGround;
  	public static Block scorchedCobblestone;	
 	
-	public static void BlockInformation() {
+	public static void init() {
 			
 		Charred_Ground = new Draco_Animus_ModForgeBlocks(Material.grass).setHardness(1.0F).setResistance(3.0F).setBlockName("charredGround").setCreativeTab(OthersDraco_Animus.TabDraco_Animus);
 		Scorched_Cobblestone = new Draco_Animus_ModForgeBlocks(Material.rock).setHardness(5.0F).setResistance(3.0F).setBlockName("scorchedCobblestone").setCreativeTab(OthersDraco_Animus.TabDraco_Animus);
