665aec8b6d72a6d7f800a7176377fbc72e2e5c25
MCrafted <zacharynwogwugwu@hotmail.co.uk>
2015-01-07 12:01:48 +0000

    Update packages
    The Core() constructor was not needed.

--- src/main/java/net/draco/src/Core.java
+++ src/main/java/net/draco/src/Core.java
@@ -1,16 +1,12 @@
 package mod.draco_animus.main;
 
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 	
 @Mod(modid = "draco_animus", name = "Draco Animus", version = "v0.03")
 public class Core {
-
-	public Core() {
-	
-	}
 		@EventHandler
 	public static void PreInit(FMLPreInitializationEvent event) {
 			Blocks.BlockInformation();
