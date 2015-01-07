769c6e641b28c34ca031af4a38cb435507272f37
MCrafted <zacharynwogwugwu@hotmail.co.uk>
2015-01-07 12:12:40 +0000

    Update BlockScorchedCobblestone.java

--- src/main/java/net/draco/src/BlockScorchedCobblestone.java
+++ src/main/java/net/draco/src/BlockScorchedCobblestone.java
@@ -1,1 +1,8 @@
-//tbd
+package mod.draco_animus.main;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+public class BlockScorchedCobblestone extends Block {
+public BlockScorchedCobblestone(Material.rock material) {
+super(material);
+}
+}
