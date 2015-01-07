f6660b2b6369fe7112f401d47660c4486e4066ed
MCrafted <zacharynwogwugwu@hotmail.co.uk>
2015-01-07 09:53:24 +0000

    Update DracoAnimus.java

--- src/main/java/net/draco/src/DracoAnimus.java
+++ src/main/java/net/draco/src/DracoAnimus.java
@@ -1,2 +1,8 @@
 //Add code here.
 package net.draco.src;
+@Mod(modid = "draco", version = "0.02", name = "Draco Animus")
+public class DracoAnimus {
+  public void init(FMLInitializationEvent event) {
+    Blocks.init()
+  }
+}
