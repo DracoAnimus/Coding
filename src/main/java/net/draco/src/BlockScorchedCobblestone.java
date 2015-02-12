9154f767a939db6a43c9447b9a796db6ee07ba60
wildbill22 <billwerth2@msn.com>
2015-02-11 20:56:35 -0800

    Complete Reorginazation

--- src/main/java/net/draco/src/BlockScorchedCobblestone.java
+++
@@ -1,8 +0,0 @@
-package mod.draco_animus.main;
-import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
-public class BlockScorchedCobblestone extends Block {
-public BlockScorchedCobblestone(Material.rock material) {
-super(material);
-}
-}
---
+++ src/main/java/net/wildbill22/draco/blocks/ModBlocks.java
@@ -0,0 +1,19 @@
+package net.wildbill22.draco.blocks;
+
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.block.Block;
+
+public class ModBlocks {
+	
+	public static Block charredGround;
+ 	public static Block scorchedCobblestone;	
+	
+	public static void init() {
+			
+		charredGround = new BlockCharredGround();
+		scorchedCobblestone = new BlockScorchedCobblestone();
+				
+		GameRegistry.registerBlock(charredGround, "charredGround");
+		GameRegistry.registerBlock(scorchedCobblestone, "scorchedCobblestone");
+	}
+}
