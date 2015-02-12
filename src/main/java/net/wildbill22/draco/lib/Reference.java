9a4d7e085d9d7fd54c361f3e5097ccf5caf133be
wildbill22 <billwerth2@msn.com>
2015-02-11 21:39:35 -0800

    Major Reorganization

--- src/main/java/net/wildbill22/draco/lib/Reference.java
+++
@@ -1,8 +0,0 @@
-package net.wildbill22.draco.lib;
-
-public class Reference 
-{
-	public static final String MODID = "wildbill22_draco";
-	public static final String NAME = "Draco Animus";
-	public static final String VERSION = "v0.0.3";	
-}
