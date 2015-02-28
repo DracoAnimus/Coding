ff54c364248312382786ad15d1d84f308ebea423
wildbill22 <billwerth2@msn.com>
2015-02-28 10:00:52 -0800

    Fix proxies

--- src/main/java/net/wildbill22/draco/proxies/ClientProxy.java
+++ src/main/java/net/wildbill22/draco/proxies/ClientProxy.java
@@ -1,6 +1,8 @@
 package net.wildbill22.draco.proxies;
 
 import net.minecraft.client.model.ModelBiped;
+import net.minecraft.client.renderer.entity.RenderArrow;
+import net.minecraft.entity.projectile.EntityArrow;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.MinecraftForgeClient;
 import net.wildbill22.draco.blocks.ModBlocks;
@@ -9,12 +11,16 @@ import net.wildbill22.draco.entities.dragons.EntityCreeperDragon;
 import net.wildbill22.draco.entities.dragons.EntitySilverDragon;
 import net.wildbill22.draco.entities.hostile.EntityGuard;
 import net.wildbill22.draco.items.ModItems;
+import net.wildbill22.draco.items.weapons.ModWeapons;
 import net.wildbill22.draco.models.ModelCreeperDragon;
 import net.wildbill22.draco.models.ModelSilverDragon;
+import net.wildbill22.draco.render.BowRender;
+import net.wildbill22.draco.render.CrossbowRender;
 import net.wildbill22.draco.render.RenderCreeperDragon;
 import net.wildbill22.draco.render.RenderGuard;
 import net.wildbill22.draco.render.RenderSilverDragon;
 import net.wildbill22.draco.render.RenderSpear;
+import net.wildbill22.draco.render.SpearItemRender;
 import net.wildbill22.draco.render.TemporaryHoardRenderer;
 import net.wildbill22.draco.render.item.ItemRendererTemporaryHoard;
 import net.wildbill22.draco.tile_entity.TileEntityTemporaryHoard;
@@ -38,11 +44,17 @@ public class ClientProxy extends CommonProxy
 		
 		// Throwable entities
 		RenderingRegistry.registerEntityRenderingHandler(EntitySpear.class, new RenderSpear(ModItems.spear));
+		RenderingRegistry.registerEntityRenderingHandler(EntityArrow.class, new RenderArrow());
 
 		// Special one for hoard
 		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityTemporaryHoard.class, new TemporaryHoardRenderer());
 		MinecraftForgeClient.registerItemRenderer(Item.getItemFromBlock(ModBlocks.temporaryHoard), 
 				new ItemRendererTemporaryHoard());
+
+		//Items
+		MinecraftForgeClient.registerItemRenderer(ModWeapons.longBow, new BowRender());
+		MinecraftForgeClient.registerItemRenderer(ModWeapons.crossbow, new CrossbowRender());
+		MinecraftForgeClient.registerItemRenderer(ModItems.spear, new SpearItemRender());
 	}
 	
 	@Override
